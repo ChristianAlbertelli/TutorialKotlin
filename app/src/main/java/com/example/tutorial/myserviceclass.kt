955ee8e61db993bc210c02a89060d9ec1fe4a734
ChristianAlbertelli <73280725+ChristianAlbertelli@users.noreply.github.com>
2020-11-05 16:58:16 +0000

    first try

---
+++ app/src/main/java/com/example/tutorial/myserviceclass.kt
@@ -0,0 +1,17 @@
+package com.example.tutorial
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.widget.Toast
+
+class MyService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+
+
+
+
+
+    }
+}
